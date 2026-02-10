e8cde0bbd4bbddd7dd5e1c36139a77948fb1ea4d
2484868 <tomasdionne@icloud.com>
2026-02-09 21:41:46 -0500

    Refactor test.py to initialize image parameters and implement coordinate calculation for cropping

--- dataset-generation/test.py
+++ dataset-generation/test.py
@@ -2,15 +2,15 @@
 import os.path
 
 from PIL import Image, ImageDraw
-initiales_x = [         ]
-initiales_y = [         ]
-cell_width = [          ]
-cell_height = []
-margin_x = []
-margin_y_top = []
-margin_y_bottom = []
-n_cells_x = []
-n_cells_y = []
+initiales_x = [48,         ] # mettre les autres valeurs pour les images 3-4-5-6-7
+initiales_y = [49,         ]
+cell_width = [32,          ]
+cell_height = [36, ]
+margin_x = [4, ]
+margin_y_top = [4, ]
+margin_y_bottom = [4, ]
+n_cells_x = [20, ]
+n_cells_y = [22,]
 for n in range(2,8): 
     dataset_path = f"dataset/page-{n}.png"
     img_base_save_path = f"dataset/generate_imgs_p{n}/"
@@ -31,6 +31,22 @@ for n in range(2,8):
                         os.makedirs(output_dir)
 
                     img_resize.save(save_path)
+
+    def get_coordinates():
+        coords = []
+        for j in range(n_cells_y[n-2]):
+            for i in range(n_cells_x[n-2]):
+                x, y = initiales_x[n-2] + i * cell_width[n-2], initiales_y[n-2] + j * cell_height[n-2]
+                x_0, x_1, y_0, y_1 = x + margin_x[n-2], x + cell_width[n-2] - margin_x[n-2], y + margin_y_top[n-2], y + cell_height[n-2] - margin_y_bottom[n-2]
+            coords.append([(x_0, y_0), (x_1, y_1)])
+        return coords
+    def place_points():
+        with Image.open(dataset_path) as img:
+            draw = ImageDraw.Draw(img)
+            coords = get_coordinates()
+            for c in coords:
+                draw.rectangle(c, outline="green", width=1)   #est ce que ca va faire les rectangles sur les 6 pages ? 
+    place_points()
 
 
 
