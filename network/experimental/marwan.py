eb1ec515939dd8eb0aa738e4842f2104b83d89df
mwn02 <mwn.boullette@gmail.com>
2026-02-19 10:24:41 -0500

    imported mnist

---
+++ network/experimental/marwan.py
@@ -0,0 +1,11 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Download training data
+training_data = datasets.MNIST(root="data", train=True, download=True, transform=ToTensor())
+
+# download test data
+test_data = datasets.MNIST(root="data", train=False, download=True, transform=ToTensor())
