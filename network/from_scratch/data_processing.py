289c6a4aa44bc8847a7ea1ae26039643ab22d3b9
mwn02 <mwn.boullette@gmail.com>
2026-03-05 10:47:20 -0500

    added mnist into the scratch nn, for testing, and it works!

--- network/from_scratch/data_processing.py
+++ network/from_scratch/data_processing.py
@@ -2,16 +2,13 @@ import numpy as np
 from PIL import Image, ImageOps
 import random
 import os
+from torchvision import datasets, transforms
 
-def get_network_data(base_path, data_ratio):
+def get_emoji_data(base_path):
     """
-    Retourne les données d'entrainement et de test, pour un dossier d'images donné.
-
-    La valeur retournée est: training_data, test_data, training_labels, test_labels.
-    Les étiquettes sont des vecteurs indicateurs (one-hot vectors)
+    Retourne les listes de données d'entrainement X, et la liste d'étiquer y.
     
-    Le data_ratio est le % des données allouées au training_data, et le
-    reste au test_data
+    La valeur retournée est X, y
     """
 
     all_images = []
@@ -60,25 +57,75 @@ def get_network_data(base_path, data_ratio):
     # 4. Conversion des listes en NumPy arrays, puis mélange
     X = np.array(all_images, dtype=np.float32)
     y = np.array(all_labels, dtype=np.int64)
+    return X, y
 
+def get_shuffled_data(X, y, num_classes, split_ratio):
+    """
+    Retroune les données mélangées d'entrainement et de test.
+
+    La valeur retournée est: X_train, X_test, y_train, y_test
+    """
     indices = np.arange(len(X))
     np.random.shuffle(indices)
 
     X = X[indices]
     y = y[indices]
 
-    split = int(data_ratio * len(X))
+    split = int(split_ratio * len(X))
 
-    train_data = X[:split]
-    train_labels = y[:split]
+    X_train = X[:split]
+    y_train = y[:split]
 
-    test_data = X[split:]
-    test_labels = y[split:]
-
-    num_classes = len(np.unique(y))
+    X_test = X[split:]
+    y_test = y[split:]
 
     # One-hot encoding pour bien marcher avec softmax et cross-entropy
-    train_labels_hot = np.eye(num_classes)[train_labels]
-    test_labels_hot = np.eye(num_classes)[test_labels]
+    y_train_hot = np.eye(num_classes)[y_train]
+    y_test_hot = np.eye(num_classes)[y_test]
 
-    return train_data, test_data, train_labels_hot, test_labels_hot
+    return X_train, X_test, y_train_hot, y_test_hot
+
+def load_mnist_data():
+    """
+    Retourne les données d'entrainement et de test (ainsi que leur étiquette) du MNIST dataset.
+
+    La valeur retournée est: X_train, X_test, y_train, y_test
+    """
+    transform = transforms.ToTensor()
+
+    train_dataset = datasets.MNIST(
+        root="./data",
+        train=True,
+        download=True,
+        transform=transform
+    )
+
+    test_dataset = datasets.MNIST(
+        root="./data",
+        train=False,
+        download=True,
+        transform=transform
+    )
+
+    X_train = train_dataset.data.numpy()
+    y_train = train_dataset.targets.numpy()
+
+    X_test = test_dataset.data.numpy()
+    y_test = test_dataset.targets.numpy()
+
+    X_train = X_train.astype(np.float32) / 255.0
+    X_test = X_test.astype(np.float32) / 255.0
+    X_train = X_train.reshape(-1, 28*28)
+    X_test = X_test.reshape(-1, 28*28)
+
+    num_classes = 10
+
+    def one_hot(y, num_classes=10):
+        oh = np.zeros((y.shape[0], num_classes), dtype=np.float32)
+        oh[np.arange(y.shape[0]), y] = 1.0
+        return oh
+
+    y_train = one_hot(y_train, num_classes)
+    y_test  = one_hot(y_test, num_classes)
+
+    return X_train, X_test, y_train, y_test
--- network/from_scratch/main.py
+++ network/from_scratch/main.py
@@ -1,43 +1,46 @@
-# Pour exécuter le script, taper dans le terminal: py -m network.main.from_scratch.main
+# Pour exécuter le script, taper dans le terminal: py -m network.from_scratch.main
 
 import numpy as np
-from network.from_scratch.data_processing import get_network_data
+from network.from_scratch.data_processing import get_emoji_data, load_mnist_data, get_shuffled_data
 import math
 
 ################## DONNÉES ##################
-base_path = "dataset/dataset-data/training-data/"
-train_data, test_data, train_labels, test_labels = get_network_data(base_path, 0.8)
+# X, y = get_emoji_data("dataset/dataset-data/training-data/")
+# X_train, X_test, y_train, y_test = get_shuffled_data(X, y, 5, 0.8)
+X_train, X_test, y_train, y_test = load_mnist_data()
 
 ################## PARAMÈTRES ##################
+dtype = np.float32
 learning_rate = 0.01
 num_epochs = 10
 batch_size = 32
 
-# Couche d'entrée (image aplatie), couches cachées, couche de sortie (5 classes)
-layers = [32*32, 128, 64, 5] 
+# Couche d'entrée (image aplatie), couches cachées, couche de sortie
+# layers = [32*32, 128, 64, 5] # pour les emojis
+layers = [28*28, 128, 64, 10] # pour mnist
 
 # attribuer des poids aléatoires pour chaque couche
-weights = [np.random.randn(layers[i+1], layers[i]) * np.sqrt(2 / layers[i]) for i in range(len(layers)-1)] # He initialization
+weights = [np.random.randn(layers[i+1], layers[i]).astype(dtype) * np.sqrt(2 / layers[i]) for i in range(len(layers)-1)] # He initialization
 # PARLER DE HE INITIALIZATION SI ON L'UTILISE, SINON, JUSTE NP.RANDOM entre [-1, 1]
 # attribuer des biais aléatoires pour chaque couche
-bias = [np.zeros(l) for l in layers[1:]]
+bias = [np.zeros(l, dtype=dtype) for l in layers[1:]]
 
 ################## RÉSEAU ##################
 def relu(Z):
     return np.maximum(0, Z)
 def relu_prime(Z):
     return (Z > 0).astype(float) # quand x = 0, ça devrait être undefined, mais il faut faire un choix pratique
-def softmax(Z):
-    exp_logits = np.exp(Z)
-    return exp_logits / np.sum(exp_logits)
+def softmax(z):
+    exp_z = np.exp(z)
+    return exp_z / (np.sum(exp_z))
 def cost_prime(predictions, label):
     return predictions - label
 
 def test():
     def forward(x):
         # Propagation avant
-        sums = [np.zeros(l, dtype=np.float32) for l in layers]
-        activations = [np.zeros(l, dtype=np.float32) for l in layers]
+        sums = [np.zeros(l, dtype=dtype) for l in layers]
+        activations = [np.zeros(l, dtype=dtype) for l in layers]
         activations[0] = x.flatten()
 
         for l in range(len(layers)-1):
@@ -55,32 +58,37 @@ def test():
         return label[np.argmax(output_layer)] == 1
 
     accuracy = 0
-    for x, label in zip(test_data, test_labels):
+    for x, label in zip(X_test, y_test):
         prediction = forward(x)
         accuracy += 1 if is_prediction_good(prediction, label) else 0
-    accuracy /= len(test_data)
+    accuracy /= len(X_test)
     print(f"Model accuracy: {accuracy*100:.2f}%")
 
 
-num_batch = math.ceil(len(train_data) / batch_size)
+num_batch = math.ceil(len(X_train) / batch_size)
 
 for epoch in range(num_epochs):
-    print(f"---------- Epoch #{epoch+1} ----------")
+
+    # mélanger les données avant la création de nouveaux mini-lots
+    perm = np.random.permutation(len(X_train))
+    X_train = X_train[perm]
+    y_train = y_train[perm]
+
     for batch in range(num_batch):
         start_index = batch*batch_size
-        end_index = min((batch+1)*batch_size, len(train_data))
+        end_index = min((batch+1)*batch_size, len(X_train))
         real_batch_size = float(end_index - start_index)
 
-        xs = train_data[start_index:end_index]
-        labels = train_labels[start_index:end_index]
+        xs = X_train[start_index:end_index]
+        labels = y_train[start_index:end_index]
 
-        gradient_w = [np.zeros((layers[i+1], layers[i])) for i in range(len(layers)-1)]
-        gradient_b = [np.zeros(l) for l in layers[1:]]
+        gradient_w = [np.zeros((layers[i+1], layers[i]), dtype=dtype) for i in range(len(layers)-1)]
+        gradient_b = [np.zeros(l, dtype=dtype) for l in layers[1:]]
 
         for x, label in zip(xs, labels):
             # Propagation avant
-            sums = [np.zeros(l, dtype=np.float32) for l in layers]
-            activations = [np.zeros(l, dtype=np.float32) for l in layers]
+            sums = [np.zeros(l, dtype=dtype) for l in layers]
+            activations = [np.zeros(l, dtype=dtype) for l in layers]
             activations[0] = x.flatten()
 
             for l in range(len(layers)-1):
@@ -104,6 +112,7 @@ for epoch in range(num_epochs):
         for i in range(len(layers)-1):
             weights[i] -= learning_rate * gradient_w[i] / real_batch_size
             bias[i] -= learning_rate * gradient_b[i] / real_batch_size
+    print(f"---------- Epoch #{epoch+1} ----------")
     test()
 
 # save model avec np.save
